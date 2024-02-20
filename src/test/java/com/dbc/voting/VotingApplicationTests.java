264dd2bf9f8b99ef9b9ab6f5cdbbde5dfad4eee4
Leonardo Mayca <leonardo.trevisan@seti.com.br>
2024-02-19 23:10:21 -0300

    Criação do projeto de gerenciamento de sessões de votação em cooperativas com Spring Boot
    Adicionadas configurações iniciais do projeto, incluindo:
    - Configuração do projeto Spring Boot usando o Spring Initializr
    - Definição das dependências do projeto (Spring Web, Spring Data JPA, MySQL Driver, Swagger)
    - Configuração do arquivo application.properties para conexão com o banco de dados MySQL
    - Inicialização do repositório Git e commit inicial

---
+++ src/test/java/com/dbc/voting/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
